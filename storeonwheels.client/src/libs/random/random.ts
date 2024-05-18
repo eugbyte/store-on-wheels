e75bb711ca85cca33eb4a1453e8ec98c47e895e4
eugene <eugenetham1994@gmail.com>
2024-05-18 17:43:32 +0800

    chore: rebase

--- storeonwheels.client/src/libs/random/random.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Returns a random number between min (inclusive) and max (exclusive)
- */
-export function getRandomNum(min: number, max: number): number {
-  return Math.random() * (max - min) + min;
-}
