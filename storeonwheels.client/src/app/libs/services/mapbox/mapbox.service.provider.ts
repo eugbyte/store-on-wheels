056db388cac960ddc946ae4f8207c0b28d3e7298
Eugene <eugenetham1994@gmail.com>
2024-05-22 16:48:36 +0800

    chore: prettier (#5)
    * chore: prettier
    
    * fix: fmt

---
+++ storeonwheels.client/src/app/libs/services/mapbox/mapbox.service.provider.ts
@@ -0,0 +1,9 @@
+import { Provider } from "@angular/core";
+import { MapboxService } from "./mapbox.service";
+
+const mapboxToken = "pk.eyJ1IjoiZXVnYnl0ZSIsImEiOiJjbHZydWMza3EwZWF0MnFvOTJzcnF5b3U2In0.6yqAJUK9dqvffd5Rcg0-uA";
+
+export const mapboxProvider: Provider = {
+  provide: MapboxService,
+  useFactory: () => new MapboxService(mapboxToken),
+};
---
+++ storeonwheels.client/src/app/libs/services/message-hub/message-hub.provider.ts
@@ -0,0 +1,9 @@
+import { MessageHubService } from "./message-hub.service";
+import { Provider } from "@angular/core";
+import { hubConnection } from "./create-connection";
+import { MathService } from "~/app/libs/services/math/math.service";
+
+export const messageHubProvider: Provider = {
+  provide: MessageHubService,
+  useFactory: () => new MessageHubService(new MathService(), hubConnection),
+}
