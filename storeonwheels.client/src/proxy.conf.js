574202f85a1c0f2b102eeac1d94bd932b37c8351
eugene <eugenetham1994@gmail.com>
2024-07-04 21:21:25 +0800

    fix: esproj.js

--- storeonwheels.client/src/proxy.conf.js
+++ storeonwheels.client/src/proxy.conf.js
@@ -1,20 +1,12 @@
 const { env } = require("process");
 
-function getBackendUrl() {
-  let target = "https://localhost:7108";
-  if (env.ASPNETCORE_HTTPS_PORT) {
-    target = `https://localhost:${env.ASPNETCORE_HTTPS_PORT}`;
-  } else if (env.ASPNETCORE_URLS) {
-    target = env.ASPNETCORE_URLS.split(";")[0];
-  }
-  console.log({ target });
-  return target;
-}
+const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
+  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7108';
 
 const PROXY_CONFIG = [
   {
     context: ["^/api"],
-    target: getBackendUrl(),
+    target,
     secure: false, // ignore lack of SSL cert
     changeOrigin: true, // prevents CORs
     logLevel: "debug",
