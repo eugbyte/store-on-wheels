11e5f18f5d8e3304940bfda4647c3a6187c5872d
Eugene <eugenetham1994@gmail.com>
2024-07-31 07:41:25 -0700

    test: broadcast annonymous (#30)

---
+++ tests/StoreOnWheels.Server.Test.Integration/Controllers/Geohubs/TestGeohubClient.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using Newtonsoft.Json;
+using StoreOnWheels.Server.Libs.Shared.Models;
+using Xunit.Abstractions;
+
+namespace StoreOnWheels.Server.Test.Integration.Controllers.Geohubs;
+
+public class TestGeohubClient(
+	CustomWebAppFactory<Program> factory,
+	ITestOutputHelper Logger
+	) : IClassFixture<CustomWebAppFactory<Program>>, IAsyncLifetime {
+	private record MessageInfo(string User, string Message);
+	private HubConnection connection = factory.HubConnection();
+
+	public async Task InitializeAsync() {
+		connection = factory.HubConnection();
+		await connection.StartAsync();
+	}
+
+	public async Task DisposeAsync() {
+		await connection.DisposeAsync();
+	}
+
+	[Fact]
+	public async Task Broadcast_Vendor_Position_Annonymously_Should_Broadcast_Position() {
+		GeolocationPosition geoInfo = new() {
+			VendorId = connection.ConnectionId ?? "",
+			Vendor = {
+				Id = connection.ConnectionId ?? "",
+				DisplayName = "",
+			}
+		};
+		string str = JsonConvert.SerializeObject(geoInfo);
+		Exception? error = null;
+		try {
+			await connection.SendAsync("BroadcastVendorPositionAnonymously", "vendor1", str);
+		} catch (Exception ex) {
+			Logger.WriteLine(ex.Message); 
+			error = ex;
+		}
+		Assert.Null(error);
+	}
+}
