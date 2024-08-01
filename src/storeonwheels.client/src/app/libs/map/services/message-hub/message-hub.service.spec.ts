0410b9fb241741f96d86a6ae3a90f3dc6aaedab6
eugene <eugenetham1994@gmail.com>
2024-08-01 21:27:45 +0800

    test: ui

--- src/storeonwheels.client/src/app/libs/map/services/message-hub/message-hub.service.spec.ts
+++ src/storeonwheels.client/src/app/libs/map/services/message-hub/message-hub.service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from "@angular/core/testing";
 
 import { MessageHubService } from "./message-hub.service";
+import { HUB_CONNECTION, hubConnection } from "./message-hub.provider";
 
 describe("MessageHubService", () => {
   let service: MessageHubService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: HUB_CONNECTION, useValue: hubConnection },
+      ]
+    });
     service = TestBed.inject(MessageHubService);
   });
 
