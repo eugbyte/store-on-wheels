5116b219062ff25329ee37910bd89536448c07e2
eugene <eugenetham1994@gmail.com>
2024-08-05 16:50:31 +0800

    Revert "chore: pwa"
    This reverts commit 59d69a1c19b9fa9265c18baef98cfbfebd43972b.

--- src/storeonwheels.client/src/app/app.config.ts
+++ src/storeonwheels.client/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, isDevMode } from "@angular/core";
+import { ApplicationConfig } from "@angular/core";
 import { provideRouter, withComponentInputBinding } from "@angular/router";
 import { routes } from "./app.routes";
 import {
@@ -6,15 +6,11 @@ import {
   withInterceptorsFromDi,
 } from "@angular/common/http";
 import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
-import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withComponentInputBinding()),
     provideHttpClient(withInterceptorsFromDi()),
-    provideAnimationsAsync(), provideServiceWorker('ngsw-worker.js', {
-            enabled: !isDevMode(),
-            registrationStrategy: 'registerWhenStable:30000'
-          }),
+    provideAnimationsAsync(),
   ],
 };
