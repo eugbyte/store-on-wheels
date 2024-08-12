3b7e1f3e09e035fd114e0a8cf6ea186b4b1c8a1e
eugene <eugenetham1994@gmail.com>
2024-08-12 21:00:02 +0800

    fix: fmt

--- src/storeonwheels.client/src/app/app.routes.ts
+++ src/storeonwheels.client/src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from "@angular/router";
 import { MapPage, BroadcastPage, HealthcheckPage } from "~/app/pages/index";
 
-// lazy loading standalone components: https://v17.angular.io/guide/standalone-components#lazy-loading-and-default-exports
 export const routes: Routes = [
   { path: "map", component: MapPage, title: "Map" },
   { path: "broadcast", component: BroadcastPage, title: "Broadcast" },
