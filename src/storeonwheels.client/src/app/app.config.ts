64eacad0d87c33c14c2ff235c33ad903b1cf12d3
eugene <eugenetham1994@gmail.com>
2024-08-12 08:44:42 +0800

    chore: pwa

--- src/storeonwheels.client/src/app/app.config.ts
+++ src/storeonwheels.client/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig } from "@angular/core";
+import { ApplicationConfig, isDevMode } from "@angular/core";
 import { provideRouter, withComponentInputBinding } from "@angular/router";
 import { routes } from "./app.routes";
 import {
@@ -6,11 +6,16 @@ import {
   withInterceptorsFromDi,
 } from "@angular/common/http";
 import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
+import { provideServiceWorker } from "@angular/service-worker";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withComponentInputBinding()),
     provideHttpClient(withInterceptorsFromDi()),
     provideAnimationsAsync(),
+    provideServiceWorker("ngsw-worker.js", {
+      enabled: !isDevMode(),
+      registrationStrategy: "registerWhenStable:30000",
+    }),
   ],
 };
