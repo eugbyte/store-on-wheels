69fe4f16c6999dd5b950398e05b04ec1f4079f69
eugene <eugenetham1994@gmail.com>
2024-08-17 22:18:12 +0800

    fix: docker

--- infra/Terraform/container-app.tf
+++ infra/Terraform/container-app.tf
@@ -9,16 +9,16 @@ resource "azurerm_container_registry" "acr" {
 }
 
 locals {
-  image = "storeonwheelsserver:latest"
+  image = "storeonwheelsserver"
 }
 
 resource "null_resource" "build_docker_image" {
   provisioner "local-exec" {
     environment = {
-      "IMAGE" = local.image
+      "IMAGE" = "${local.image}:latest"
     }
     working_dir = "../.."
-    command = "docker compose build"
+    command     = "docker compose build"
   }
   depends_on = [azurerm_container_registry.acr]
 }
@@ -26,44 +26,45 @@ resource "null_resource" "build_docker_image" {
 resource "null_resource" "push_docker_image" {
   provisioner "local-exec" {
     command = <<-EOT
-      az acr login --name ${azurerm_container_registry.acr.name}
-      docker push "${azurerm_container_registry.acr.login_server}/${local.image}"
+        az acr login --name ${azurerm_container_registry.acr.name}
+        docker tag storeonwheelsserver "${azurerm_container_registry.acr.login_server}/${local.image}"
+        docker push "${azurerm_container_registry.acr.login_server}/${local.image}:latest"
     EOT
   }
   depends_on = [null_resource.build_docker_image]
 }
 
-resource "azurerm_container_app_environment" "env" {
-  name                       = "ca-env-storeonwheels-prod-sea"
-  location                   = azurerm_resource_group.rg.location
-  resource_group_name        = azurerm_resource_group.rg.name
-  log_analytics_workspace_id = azurerm_log_analytics_workspace.analytics.id
-}
+# resource "azurerm_container_app_environment" "env" {
+#   name                       = "ca-env-storeonwheels-prod-sea"
+#   location                   = azurerm_resource_group.rg.location
+#   resource_group_name        = azurerm_resource_group.rg.name
+#   log_analytics_workspace_id = azurerm_log_analytics_workspace.analytics.id
+# }
 
-resource "azurerm_container_app" "app" {
-  name                         = "ca-storeonwheels-prod-sea"
-  container_app_environment_id = azurerm_container_app_environment.env.id
-  resource_group_name          = azurerm_resource_group.rg.name
-  revision_mode                = "Single"
+# resource "azurerm_container_app" "app" {
+#   name                         = "ca-storeonwheels-prod-sea"
+#   container_app_environment_id = azurerm_container_app_environment.env.id
+#   resource_group_name          = azurerm_resource_group.rg.name
+#   revision_mode                = "Single"
 
-  template {
-    container {
-      name   = "storeonwheelsserver"
-      image  = "${azurerm_container_registry.acr.login_server}/${local.image}"
-      cpu    = 0.5
-      memory = "0.5Gi"
-    }
-  }
+#   template {
+#     container {
+#       name   = "storeonwheelsserver"
+#       image  = "${azurerm_container_registry.acr.login_server}/${local.image}"
+#       cpu    = 0.5
+#       memory = "1Gi"
+#     }
+#   }
 
-  ingress {
-    allow_insecure_connections = false
-    external_enabled           = true
-    target_port                = 4000
-    # ws connection not distributed to message queues yet
-    traffic_weight {
-      percentage = 100
-    }
-  }
+#   ingress {
+#     allow_insecure_connections = false
+#     external_enabled           = true
+#     target_port                = 4000
+#     # ws connection not distributed to message queues yet
+#     traffic_weight {
+#       percentage = 100
+#     }
+#   }
 
-  depends_on = [null_resource.push_docker_image]
-}
+#   depends_on = [null_resource.push_docker_image]
+# }
